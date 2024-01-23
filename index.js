78114f3714e18e57e96abffcde5206f748a0f461
ISxaNI <jasonliu99999@yahoo.com>
2024-01-22 21:39:43 -0800

    Final Edit

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+/////////////////////Function Section/////////////////////////////
+function newImage(src, left, bottom) {
+    let image = document.createElement('img');
+    image.src = src;
+    image.style.position = 'fixed';
+    image.style.left = left;
+    image.style.bottom = bottom;
+document.body.append(image);
+return image;
+}
+
+function newItem(src, left, bottom){
+    let item = newImage(src, left, bottom)
+    item.addEventListener('dblclick', function(){
+        item.remove();
+    });
+    return item;
+};
+
+///////////////////newImage section/////////////////////
+newImage('assets/green-character.gif', '100px', '100px')
+newImage('assets/pine-tree.png', '450px', '200px')
+newImage('assets/tree.png', '200px', '300px')
+newImage('assets/pillar.png', '350px', '100px')
+newImage('assets/crate.png', '150px', '200px')
+newImage('assets/well.png', '500px', '425px')
+///////////////////////////newItem Section///////////////////////////////
+newItem('assets/sword.png', '500px', '405px')
+newItem('assets/shield.png', '600px', '185px')
+newItem('assets/staff.png', '600px', '100px') 
+
+
